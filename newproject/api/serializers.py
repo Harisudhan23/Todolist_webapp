feaf57f056cb0335d39ae12e6ec557df98749d9f
Harihara Sudhan <nhsharsudhan2307@gmail.com>
2024-11-26 15:55:54 +0530

    SE Project

--- newproject/api/serializers.py
+++ newproject/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
-from .models import user
+from .models import Task
 
 class Taskserializer(serializers.ModelSerializer):
     class Meta:
-        model = user
+        model = Task
         fields = '__all__'
--- newproject/api/views.py
+++ newproject/api/views.py
@@ -2,16 +2,16 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Task
-from serializers import TaskSerializer
+from .serializers import Taskserializer
 
 class TaskListCreateView(APIView):
     def get(self, request):
         tasks = Task.objects.all()
-        serializer = TaskSerializer(tasks, many=True)
+        serializer = Taskserializer(tasks, many=True)
         return Response(serializer.data)
 
     def post(self, request):
-        serializer = TaskSerializer(data=request.data)
+        serializer = Taskserializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -28,7 +28,7 @@ class TaskDetailView(APIView):
         task = self.get_object(pk)
         if not task:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = TaskSerializer(task, data=request.data)
+        serializer = Taskserializer(task, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
