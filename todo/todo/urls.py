bf57f012534bae02183387bc33cbb5caa55c6553
Harihara Sudhan <nhsharsudhan2307@gmail.com>
2024-11-26 23:12:26 +0530

    SE Project

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sign/', views.signup),
+    path('login/', views.login),
 ]
